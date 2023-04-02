2ab005a27aa6e54d93deb1f2154b8b88f06413f1
Genacud <bgennadij265@icloud.com>
2023-04-02 10:15:00 +0300

    ex_2-done

--- PythonApplication_str_.py
+++ PythonApplication_str_.py
@@ -1,1 +1,11 @@
 
+enter = input('Enter text: ')
+l_c = 0
+n_c = 0
+for i in enter:
+    if i.isalpha():
+        l_c+=1
+    if i.isdigit():
+        n_c+=1
+print('Кількість літер: ', l_c)
+print('Кількість цифр: ', n_c)

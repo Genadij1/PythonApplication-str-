bff4c0c8cfeed6e726975b3068ba20eb75c87dd4
Genacud <bgennadij265@icloud.com>
2023-04-11 16:22:37 +0300

    ex_5-done

--- PythonApplication_str_.py
+++ PythonApplication_str_.py
@@ -1,1 +1,6 @@
 
+B = input("Введіть рядок: ")
+research = input("Введіть слово для пошуку: ")
+zamina = input("Введіть слово для заміни: ")
+new_b = B.replace(research, zamina)
+print("Результат:", new_b)
